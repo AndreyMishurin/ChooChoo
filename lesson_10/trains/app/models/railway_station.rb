f684b9e5ac95addd99be8f354a5fd28459ca96be
Andrey <asmishurin@gmail.com>
2017-03-21 15:13:16 +0300

    feature_mod

--- lesson_10/trains/app/models/railway_station.rb
+++ lesson_10/trains/app/models/railway_station.rb
@@ -1,6 +1,24 @@
 class RailwayStation < ActiveRecord::Base
+  validates :title, presence: true
+
+  has_many :trains, foreign_key: :current_station_id
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
-  has_many :trains, foreign_key: :current_station_id
-end
 
+  scope :ordered, -> { select('railway_stations.*, railway_stations_routes.position').joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
+
+  def update_position(route, position)
+    station_route = station_route(route)
+    station_route.update(position: position) if station_route
+  end
+
+  def position_in(route)
+    station_route(route).try(:position)
+  end
+
+  protected
+
+  def station_route(route)
+    @station_route ||= railway_stations_routes.where(route: route).first
+  end
+end
